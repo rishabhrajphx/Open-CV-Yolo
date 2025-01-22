f985a6bcfa426c7fe96791836454a4edd42141dc
rishabhrajphx <rishabhnewmob@gmail.com>
2025-01-22 10:45:05 -0700

    Test images and webcam interface

---
+++ yolo.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import cv2
+
+# Load the YOLOv8 model
+model = YOLO("yolov8s.pt")
+
+# Open webcam (0 is usually the default webcam)
+cap = cv2.VideoCapture(0)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
